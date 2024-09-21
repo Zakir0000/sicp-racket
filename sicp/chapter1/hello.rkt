82dfd5bfbdd84ea4451514b1183400f485af637b
Zakir0000 <zakirkhunkaev@gmail.com>
2024-09-21 20:38:03 +0500

    training Racket

--- sicp/chapter1/hello.rkt
+++ sicp/chapter1/hello.rkt
@@ -1,6 +1,22 @@
 #lang racket
 
-(define (same-parity? x y) (equal? (even? x) (even? y)))
+(define (do-today n)
+   (cond
+        [(and (> n 0) (< n 6)) "work"]
+        [(and (> n 5) (< n 8)) "rest"]
+        [(equal? n #f) "???"]
+        
+        [else "???"]))
 
-
-(same-parity? 6 6)
+(do-today 1)
+(do-today 2)
+(do-today 3)
+(do-today 4)
+(do-today 5)
+(do-today 6)
+(do-today 7)
+(do-today 0)
+(do-today -1)
+(do-today 10)
+(do-today #f)
+(do-today "oops")
